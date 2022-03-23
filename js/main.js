32db3ef2c4a4b7c6ce3edf2e6819692709a5576b
Mitja Krašovec <mitja.krasovec@specto.si>
2022-03-23 15:32:04 +0100

    added an API call for home quote

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 const dailyImageUrl =
 	'https://api.nasa.gov/planetary/apod?api_key=whyzw93A2cmHzchDRiHf514XoTp0yRe0RhqZ2qwg';
 
-let fetchImage = fetch(dailyImageUrl)
+const dQuote = 'https://quotes.rest/qod';
+
+fetch(dailyImageUrl)
 	.then((response) => {
 		return response.json();
 	})
@@ -9,6 +11,18 @@ let fetchImage = fetch(dailyImageUrl)
 		astroImage(data.url);
 	});
 
+fetch(dQuote)
+	.then((response) => {
+		return response.json();
+	})
+	.then((quoteData) => {
+		dailyQuote(quoteData.contents.quotes[0].quote);
+	});
+
 function astroImage(image_url) {
 	document.getElementById('astro-daily').src = image_url;
+}
+
+function dailyQuote(quote) {
+	document.getElementById('quote-daily').innerHTML = quote;
 }
