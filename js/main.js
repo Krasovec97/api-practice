d72ba9eeb8d35f9da1c257b70b4c021d182afa69
Mitja Krašovec <mitja.krasovec@specto.si>
2022-04-06 12:47:29 +0200

    Working Timer

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ fetch(API.NEOWS)
 	.then((response) => response.json())
 	.then((data) => {
 		let value = '';
-		filterNeos(data.near_earth_objects).forEach((object) => {
+		filterNeos(data.near_earth_objects).forEach((object, index) => {
 			const closestDate = closestToToday(object.close_approach_data);
 			const diameterMin = Math.round(object.estimated_diameter.kilometers.estimated_diameter_min);
 			const diameterMax = Math.round(object.estimated_diameter.kilometers.estimated_diameter_max);
@@ -35,7 +35,7 @@ fetch(API.NEOWS)
 					<div class="diameter">Estimated diameter from: ${diameterMin} to ${diameterMax} Km</div>
 					<div class="next-approach">
 						Next Approach in: <br>
-						<div class="next-apporach__date" id="date-display"></div>
+						<div class="next-apporach__date">${closestDate}</div>
 						Or on: <br>${new Date(closestDate)}
 					</div>
 					<p>See all encounters with ${object.name_limited}:</p>
@@ -45,10 +45,10 @@ fetch(API.NEOWS)
 
 			document.querySelector('#neo__card-container').innerHTML = value;
 
-			setInterval(updateClock(closestDate), 1000);
+			setInterval(() => updateClock(closestDate, index), 1000);
 		});
-	});
-//.catch((err) => handleError(err));
+	})
+	.catch((err) => handleError(err));
 
 function handleError(error) {
 	console.error('Error: ' + error);
@@ -94,7 +94,8 @@ function closestToToday(neoDate) {
 }
 
 function getTimeRemaining(date) {
-	const total = Date.parse(date) - Date.parse(today);
+	const now = new Date();
+	const total = Date.parse(date) - Date.parse(now);
 	const seconds = Math.floor((total / 1000) % 60);
 	const minutes = Math.floor((total / 1000 / 60) % 60);
 	const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
@@ -108,9 +109,13 @@ function getTimeRemaining(date) {
 	};
 }
 
-function updateClock(date) {
+function updateClock(date, index) {
 	const calculatedTime = getTimeRemaining(date);
 
-	document.querySelector('#date-display').innerHTML = `${calculatedTime.days} days, ${calculatedTime.hours} hours,<br>
-	${calculatedTime.minutes} minutes, ${calculatedTime.seconds} seconds.`;
+	const element = document.querySelectorAll('.next-apporach__date')[index];
+
+	if (element) {
+		element.innerHTML = `${calculatedTime.days} days, ${calculatedTime.hours} hours,<br>
+		${calculatedTime.minutes} minutes, ${calculatedTime.seconds} seconds.`;
+	}
 }
